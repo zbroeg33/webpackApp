d9c2b02dc203080a47e87df2f850e1368f836846
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2019-08-25 12:11:44 -0600

    init push?

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+module.exports = {
+    entry: [
+      './src/index.js',
+      './src/scss/index.scss'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "script-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[name]_[local]_[hash:base64]",
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  };
